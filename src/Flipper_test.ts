f5d55757639b43e8d75c9e7591471fcd5cb11202
Jonathan Hoyt <hoyt@github.com>
2017-05-10 21:04:05 -0700

    Refactor tests to setup Flipper instance in beforeEach

--- src/Flipper_test.ts
+++ src/Flipper_test.ts
@@ -2,27 +2,29 @@ import Flipper from './Flipper'
 import { assert } from 'chai'
 import 'mocha'
 
+let flipper: Flipper
+
 describe('Flipper', () => {
+  beforeEach(() => {
+    flipper = new Flipper()
+  })
+
   it('should start with feature in disabled state', () => {
-    const flipper = new Flipper()
     assert.equal(flipper.isFeatureEnabled('feature-1'), false)
   })
 
   it('can enable feature', () => {
-    const flipper = new Flipper()
     flipper.enableFeature('feature-1')
     assert.equal(flipper.isFeatureEnabled('feature-1'), true)
   })
 
   it('can disable an enabled feature', () => {
-    const flipper = new Flipper()
     flipper.enableFeature('feature-1')
     flipper.disableFeature('feature-1')
     assert.equal(flipper.isFeatureEnabled('feature-1'), false)
   })
 
   it('can enable one feature without enabling another feature', () => {
-    const flipper = new Flipper()
     flipper.enableFeature('feature-1')
     assert.equal(flipper.isFeatureEnabled('feature-1'), true)
     assert.equal(flipper.isFeatureEnabled('feature-2'), false)
